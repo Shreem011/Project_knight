557afed05827c00a8dcca8dff3a2feec057fafbf
Shreem <73521919+Shreem011@users.noreply.github.com>
2020-11-25 12:07:17 +0545

    Add files via upload

---
+++ winScreen.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include<iostream>
+using namespace std;
+
+class YouWin
+{
+public:
+    YouWin(float width , float height);
+    ~YouWin(){//...
+    }
+
+
+
+private:
+    sf::Font fontsss;
+	sf::Texture texturesss;
+};
+
+
